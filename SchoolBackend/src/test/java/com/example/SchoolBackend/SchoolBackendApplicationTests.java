8b6fd8d0696f672b36a295727aca44dda382d627
asemega <semegaaliou4@gmail.com>
2024-05-02 14:05:42 +0000

    School Management

---
+++ SchoolBackend/src/test/java/com/example/SchoolBackend/SchoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SchoolBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
